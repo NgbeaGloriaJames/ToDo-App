150e334f395f525489218949f16cc70c5c42f23c
Gloria Ngbea <ngbeagloriajames@gmail.com>
2023-12-08 23:19:53 +0100

    5th

--- main.js
+++ main.js
@@ -1,29 +1,32 @@
 const app = Vue.createApp({
-        data() {
-            return {
-                todoContent: '',
-                todos: [],
-                sN: 1,
-             }
-        },
-        methods: {
-            addTodo(){
-                this.todos.push({
-                    todoContent: this.todoContent,
-                    id: this.sN,
-                })
-                this.sN++;
-                this.todoContent = '';
-                },
-                editTodo(index) {
-                    const newContent = prompt('Edit ToDo:', this.todos[index].todoContent);
-                    if (newContent !== null) {
-                        this.todos[index].todoContent = newContent;
-                    }
-                },
-                deleteTodo(index) {
-                        this.todos.splice(index, 1);
-                },
+    data() {
+        return {
+            todoContent: '',
+            todos: [],
+            sN: 1,
         }
-        
-    })
+    },
+    methods: {
+        addTodo() {
+            this.todos.push({
+                todoContent: this.todoContent,
+                id: this.sN,
+                completed: false,
+            });
+            this.sN++;
+            this.todoContent = '';
+        },
+        editTodo(index) {
+            const newContent = prompt('Edit ToDo:', this.todos[index].todoContent);
+            if (newContent !== null) {
+                this.todos[index].todoContent = newContent;
+            }
+        },
+        deleteTodo(index) {
+            this.todos.splice(index, 1);
+        },
+        toggleDone(index) {
+            this.todos[index].completed = !this.todos[index].completed;
+        },
+    }
+});
