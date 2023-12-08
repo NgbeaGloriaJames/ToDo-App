ca0301efdf34b4f7607f1f9807744a5c2d9e6686
Gloria Ngbea <ngbeagloriajames@gmail.com>
2023-12-08 23:34:26 +0100

    new update

--- main.js
+++ main.js
@@ -4,29 +4,48 @@ const app = Vue.createApp({
             todoContent: '',
             todos: [],
             sN: 1,
-        }
+        };
     },
     methods: {
         addTodo() {
-            this.todos.push({
-                todoContent: this.todoContent,
-                id: this.sN,
-                completed: false,
-            });
-            this.sN++;
+            const trimmedContent = this.todoContent.trim();
+            if (trimmedContent !== '') {
+                this.todos.push({
+                    todoContent: trimmedContent,
+                    id: this.sN,
+                    completed: false,
+                });
+                this.sN++;
+
+                // Update local storage
+                localStorage.setItem('todos', JSON.stringify(this.todos));
+            }
             this.todoContent = '';
         },
         editTodo(index) {
             const newContent = prompt('Edit ToDo:', this.todos[index].todoContent);
             if (newContent !== null) {
                 this.todos[index].todoContent = newContent;
+
+                // Update local storage
+                localStorage.setItem('todos', JSON.stringify(this.todos));
             }
         },
         deleteTodo(index) {
             this.todos.splice(index, 1);
+
+            //   storage
+            localStorage.setItem('todos', JSON.stringify(this.todos));
         },
         toggleDone(index) {
             this.todos[index].completed = !this.todos[index].completed;
+
+            //   storage
+            localStorage.setItem('todos', JSON.stringify(this.todos));
         },
-    }
+    },
+    mounted() {
+        // Load todos from local storage after the app is mounted
+        this.todos = JSON.parse(localStorage.getItem('todos')) || [];
+    },
 });
